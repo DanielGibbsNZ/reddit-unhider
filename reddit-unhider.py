1faabf2e384d8192744a6a0d11a543c24bbb11bb
Daniel Gibbs <daniel@danielgibbs.name>
2013-07-20 15:26:00 +1200

    Made it actually work properly.

--- reddit-unhider.py
+++ reddit-unhider.py
@@ -3,20 +3,27 @@ import time
 
 username = "Username"
 password = "Password"
-time_cutoff = int(time.time()) - (60 * 60 * 24)
+days_to_keep = 1
 
+time_cutoff = int(time.time()) - (days_to_keep * 60 * 60 * 24)
 num_hidden = 0
-reddit = praw.Reddit(user_agent = "Reddit-unhider")
+after = None
+reddit = praw.Reddit(user_agent="Reddit-unhider")
 
 print "Logging in..."
 reddit.login(username, password)
 
-hidden_posts = reddit.user.get_hidden(limit = None)
-for post in hidden_posts:
-	if int(post.created_utc) >= time_cutoff:
-		continue
-	print "Unhiding [%s] ... " % post.title
-	post.unhide()
-	num_hidden += 1
+no_more = False
+while not no_more:
+	hidden_posts = reddit.user.get_hidden(limit=None, params={"after" : after})
+	no_more = True
+	for post in hidden_posts:
+		no_more = False
+		if int(post.created_utc) >= time_cutoff:
+			after = post.id
+			continue
+		print "Unhiding [%s] ... " % post.title
+		post.unhide()
+		num_hidden += 1
 
 print "\n%d posts unhidden!" % num_hidden
